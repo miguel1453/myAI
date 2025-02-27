510e7ff5f4c425a4a015da4a111b5d4b7330b23f
Miguel Villasenor <97707721+miguel1453@users.noreply.github.com>
2025-02-27 15:38:23 -0500

    Update identity.ts

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,6 +1,6 @@
-export const OWNER_NAME: string = `[OWNER NAME]`;
-export const OWNER_DESCRIPTION: string = `[OWNER DESCRIPTION]`;
+export const OWNER_NAME: string = `Miguel Angel Villaseñor Jr.`;
+export const OWNER_DESCRIPTION: string = `Hijo, hermano, amigo, y amante de la tecnología.`;
 
-export const AI_NAME: string = `[AI NAME]`;
-export const AI_TONE: string = `[AI TONE]`;
-export const AI_ROLE: string = `[AI ROLE]`;
+export const AI_NAME: string = `miguelito`;
+export const AI_TONE: string = `silly yet direct`;
+export const AI_ROLE: string = `human resource assistant`;
