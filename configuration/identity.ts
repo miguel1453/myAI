d92f124683b51a89059a617b1e461218f584a262
Miguel Villasenor <97707721+miguel1453@users.noreply.github.com>
2025-03-06 21:46:20 -0500

    done

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,6 +1,5 @@
-export const OWNER_NAME: string = `Miguel Angel Villaseñor Jr.`;
+export const OWNER_NAME: string = `Miguel Angel`;
 export const OWNER_DESCRIPTION: string = `Hijo, hermano, amigo, y amante de la tecnología.`;
-
-export const AI_NAME: string = `miguelito`;
-export const AI_TONE: string = `silly yet direct`;
+export const AI_NAME: string = `Don Mike`;
+export const AI_TONE: string = `enojon y directo`;
 export const AI_ROLE: string = `human resource assistant`;
--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,10 +1,10 @@
 import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
-export const CHAT_HEADER: string = `[CHAT HEADER]`;
-export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
-export const FOOTER_MESSAGE: string = `[FOOTER MESSAGE]`;
-export const CLEAR_BUTTON_TEXT: string = `[CLEAR BUTTON TEXT]`;
-export const PAGE_TITLE: string = `[PAGE TITLE]`;
-export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
+export const CHAT_HEADER: string = `Don Clic`;
+export const MESSAGE_PLACEHOLDER: string = `Habla con ${AI_NAME}...`;
+export const FOOTER_MESSAGE: string = `Creado por ${OWNER_NAME}`;
+export const CLEAR_BUTTON_TEXT: string = `Borrar`;
+export const PAGE_TITLE: string = `Chat con ${AI_NAME}`;
+export const PAGE_DESCRIPTION: string = `Habla con ${AI_NAME}, ${OWNER_NAME}'s AI.`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
